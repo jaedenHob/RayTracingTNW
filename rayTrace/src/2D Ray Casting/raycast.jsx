b76e7230a04e7d9fcca1f9700badbbfa49cd1bab
jaedenHob <jaedenhuxtable@gmail.com>
2024-01-07 15:01:07 -0500

    finally got a line to show through p5 wrapper for reactjs.

--- rayTrace/src/2D Ray Casting/raycast.jsx
+++ rayTrace/src/2D Ray Casting/raycast.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import { ReactP5Wrapper } from "@p5-wrapper/react";
+// import ScriptTag from  'react-script-tag';
 
 function sketch(p5) {
-    p5.setup = () => p5.createCanvas(800, 600);
+    class Boundary {
+        constructor(x1, y1, x2, y2) {
+            this.a = p5.createVector(x1, y1);
+            this.b = p5.createVector(x2, y2);
+        }
+
+        show() {
+            p5.stroke(255);
+            p5.line(this.a.x, this.a.y, this.b.x, this.b.y);
+        }
+    };
+
+    let b;
+
+    p5.setup = () => {
+        p5.createCanvas(800, 600);
+
+       b = new Boundary(300, 100, 300, 300);
+    }
 
     p5.draw = () => {
         p5.background(0);
-        // p5.push();
-        // p5.pop();
+        p5.stroke(255);
+
+        b.show();
+
+        // p5.line(50, 550, 50, 50);
     };
 }
 
 const Raycast = () => {
   return (
-    <ReactP5Wrapper sketch={sketch} />
+    <>
+        <ReactP5Wrapper sketch={sketch} />
+    </>
   )
 }
 
