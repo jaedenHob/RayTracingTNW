68c611267a1e50dd487512565a2ae0ab633d76fd
jaedenHob <jaedenhuxtable@gmail.com>
2024-01-06 21:27:56 -0500

    including my main reference material

--- rayTrace/src/App.jsx
+++ rayTrace/src/App.jsx
@@ -226,7 +226,16 @@ function App() {
     <>
       <h1>Raytracing Project</h1>
 
-      <br/>
+      
+      <p>
+        <br />After my computer graphics course in college I enjoyed the process of designing a game using all 
+        <br />the techniques that was learned. However, I felt that there was a valuable learning  experience I missed
+        <br />out on building a ray tracer. So this project will aim to do just that. My process begun by reading
+        <br />up on it in 
+        <a href='https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview'> ray tracing in one weekend</a>.  The reference material is in C++, but I will stick to what 
+        <br />I'm used to with javascript and webgl. Ray tracing is a complex subject so I don't expect
+        <br />to complete this in one weekend, but lets see I how far I can get.
+      </p>    
 
       <h1>Triangle Webgl Render</h1>
       <CanvasComponentTriangle />
