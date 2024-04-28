2a3958c7603cabd897e406f4787e0c210fd93030
jaedenHob <jaedenhuxtable@gmail.com>
2024-04-28 14:36:31 -0400

    initailization checking for any issues.

--- rayTrace/src/Components/webglTracer.jsx
+++ rayTrace/src/Components/webglTracer.jsx
@@ -640,7 +640,7 @@ const Raytrace = () => {
             in vec2 v_position;
             out vec4 fragColor;
             void main() {
-                fragColor = vec4(0., 0., 0.0, 0.0);
+                fragColor = vec4(0., 0., 0.0, 1.0);
             }`,
         }
 
